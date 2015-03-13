d6fa82f24bafdfa5be2e3a8a45a823ff7919df96
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-03-13 18:04:24 +0100

    Added functionality for sending fuel reminder emails Added template functions used for formatting fuel reminder emails to EmailController

--- mail/controller.go
+++ mail/controller.go
@@ -8,10 +8,15 @@ import (
 	"net"
 	"net/smtp"
 	"net/url"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/morpheusxaut/evepos/database"
 	"github.com/morpheusxaut/evepos/misc"
+	"github.com/morpheusxaut/evepos/models"
+
+	"github.com/dustin/go-humanize"
 )
 
 // Controller handles sending mails via a given SMTP server as required by the app
@@ -32,7 +37,7 @@ func SetupMailController(conf *misc.Configuration, db database.Connection) *Cont
 
 // SendPasswordReset sends a verification email with a password reset link to the user's given email address
 func (controller *Controller) SendPasswordReset(username string, email string, verification string) error {
-	templates := template.Must(template.New("").ParseFiles("app/templates/passwordreset.html"))
+	templates := template.Must(template.New("").Funcs(controller.TemplateFunctions()).ParseFiles("app/templates/passwordreset.html"))
 
 	data := make(map[string]interface{})
 	data["username"] = username
@@ -45,7 +50,23 @@ func (controller *Controller) SendPasswordReset(username string, email string, v
 		return err
 	}
 
-	return controller.SendEmail(email, "patchserv - Password reset", buf.String(), fmt.Sprintf("Please use the following link to reset your password: %s/login/reset/verify?email=%s&username=%s&verification=%s", controller.config.HTTPPublicURL, email, username, verification))
+	return controller.SendEmail(email, "evepos - Password reset", buf.String(), fmt.Sprintf("Please use the following link to reset your password: %s/login/reset/verify?email=%s&username=%s&verification=%s", controller.config.HTTPPublicURL, email, username, verification))
+}
+
+func (controller *Controller) SendFuelReminder(username string, email string, poses []*models.POS) error {
+	templates := template.Must(template.New("").Funcs(controller.TemplateFunctions()).ParseFiles("app/templates/fuelreminder.html"))
+
+	data := make(map[string]interface{})
+	data["username"] = username
+	data["poses"] = poses
+
+	var buf bytes.Buffer
+	err := templates.ExecuteTemplate(&buf, "fuelreminder", data)
+	if err != nil {
+		return err
+	}
+
+	return controller.SendEmail(email, "evepos - POS fuel reminder", buf.String(), fmt.Sprintf("POS fuel reminder. Check %s/poses", controller.config.HTTPPublicURL))
 }
 
 // SendEmail properly formats an email with the given data and sends it via a SMTP client
@@ -161,4 +182,35 @@ func (controller *Controller) CreateMessageBuffer(from string, to string, subjec
 	buffer.WriteString(fmt.Sprintf("--%s--\r\n", boundaryString))
 
 	return buffer
+}
+
+// TemplateFunctions prepares a map of functions to be used within templates
+func (controller *Controller) TemplateFunctions() template.FuncMap {
+	return template.FuncMap{
+		"FormatType":              func(t int64) string { return controller.FormatType(t) },
+		"FormatLocation":          func(m int64) string { return controller.FormatLocation(m) },
+		"FormatRemainingFuelTime": func(u int64, q int64) string { return controller.FormatRemainingFuelTime(u, q) },
+	}
+}
+
+func (controller *Controller) FormatType(typeID int64) string {
+	typeName, err := controller.database.QueryTypeName(typeID)
+	if err != nil {
+		return strconv.FormatInt(typeID, 10)
+	}
+
+	return typeName
+}
+
+func (controller *Controller) FormatLocation(moonID int64) string {
+	location, err := controller.database.QueryLocationName(moonID)
+	if err != nil {
+		return strconv.FormatInt(moonID, 10)
+	}
+
+	return location
+}
+
+func (controller *Controller) FormatRemainingFuelTime(usage int64, quantity int64) string {
+	return humanize.Time(time.Now().Add(time.Hour * time.Duration(quantity/usage)))
 }
